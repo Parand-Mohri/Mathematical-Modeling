d55be5a37b7c9e59116c17de7e8deeb737f5345d
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-02-21 10:08:33 +0100

    add exercise 1

--- exercise 3/Fadeev.m
+++ exercise 3/Fadeev.m
@@ -1,6 +1,10 @@
 function [b,a,S] = Fadeev(A,B,C,D)
-%FADEEV Summary of this function goes here
-%   Detailed explanation goes here
+%% Algorithm for computing transfer function from state-space representation
+%    Inputs: state-space representation by specifying A, B, C, D
+%    outputs: coefficients of the numerator polynomial b of transfer function,
+%    the coefficients of the denominator polynomial a of transfer function
+%    and a cell array S containing S1 through Sn
 
 end
+
 
