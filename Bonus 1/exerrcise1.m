2d970138262e0261a19191b90324592204775a04
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-02-22 17:18:49 +0100

    i promise exercise 1 is done

--- Bonus 1/exerrcise1.m
+++ Bonus 1/exerrcise1.m
@@ -1,7 +1,7 @@
 
-%(0, 0) / x' = 0  /   y' = -5 --> false 
-%(1, 2)  / x ' = 0 /  y' = 0  --> true 
-%(2, 1)  / x ' = 0 / y' = 0 --> true 
+%(0, 0) / x' = 0  /   y' = -5 --> NOT equilibrium
+%(1, 2)  / x ' = 0 /  y' = 0  --> equilibrium 
+%(2, 1)  / x ' = 0 / y' = 0 --> equilibrium 
 syms x y
 derX = @(x, y) 2*x - 3*x*y + y ^ 2;
 derY = @(x, y) x + y + x*y - 5;
@@ -9,11 +9,12 @@ x0 = 1;
 y0 = 2;
 
 % jocobian 
-J(x,y) = jacobian([2*x - 3*x*y + y ^ 2, x + y + x*y - 5],[x,y]);
-J(x0,y0)
-x1 = x0 + 0.0001;
-y1 = y0 + 0.0001;
-der =  J(x0,y0) * [x1;y1];
-derX1 = der(1);
-derY1 =  der(2);
+J(x,y) = jacobian([2*x - 3*x*y + y ^ 2, x + y + x*y - 5],[x,y]);    
+% initial two points around x0 and y0 
+xHat = x - x0; 
+yHat = y - y0;
+% linearisation around (x0,y0)
+derHat =  J(x0,y0) * [xHat;yHat]
+derXHat = derHat(1);
+derYHat =  derHat(2);
 
